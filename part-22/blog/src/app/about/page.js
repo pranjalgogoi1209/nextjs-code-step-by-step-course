eb6f40e93fbccce1ef4074d0947164e5b7d20fc3
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-01-21 12:21:49 +0530

    part-22 => Moduler css

---
+++ part-22/blog/src/app/about/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "@/styles/about.module.css";
+
+export default function About() {
+  return (
+    <div className={styles.main}>
+      <h1>I am about page</h1>
+    </div>
+  );
+}
---
+++ part-22/blog/src/app/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.main}>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
