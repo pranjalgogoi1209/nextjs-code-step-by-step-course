d6788bbe8b68c487b841dd89ca78718f3211d358
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-01-22 14:40:14 +0530

    part-29 static assets

---
+++ part-29/blog/src/app/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <link rel="stylesheet" href="/globalStyle.css" />
+        <script src="/common.js" />
+      </head>
+      <h1 className="home">Home page</h1>
+    </div>
+  );
+}
