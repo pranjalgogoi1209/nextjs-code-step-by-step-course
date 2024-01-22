8bdb3565de2fbd9cd075988a65c7b868a6eb08eb
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-01-22 15:54:16 +0530

    static site generation

---
+++ part-31/blog/src/app/users/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import User from "@/components/user";
+
+export default function Users() {
+  return (
+    <div>
+      <h1>Users page</h1>
+      <User />
+    </div>
+  );
+}
---
+++ part-31/blog/src/components/user.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function User() {
+  return (
+    <div>
+      <h1>I am single user</h1>
+    </div>
+  );
+}
